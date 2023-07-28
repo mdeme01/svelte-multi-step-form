41133982191bdcf042449cb4e3db9e84f70814c6
mdeme01 <se4n01@gmail.com>
2023-07-28 19:19:20 +0200

    feat: subscription store, select plan styling

--- src/lib/components/select-plan.svelte
+++ src/lib/components/select-plan.svelte
@@ -1,15 +1,74 @@
+<script lang="ts">
+	import { plans, subscription } from '$lib/stores/subscription-store';
+</script>
+
 <section>
 	<h1>Select your plan</h1>
 	<p>You have the option of monthly or yearly billing.</p>
 	<form>
-		<label for="arcade">Arcade $9/mo</label>
-		<input type="radio" name="plan" id="arcade" />
-		<label for="advanced">Advanced $12/mo</label>
-		<input type="radio" name="plan" id="advanced" />
-		<label for="pro">Pro $15/mo</label>
-		<input type="radio" name="plan" id="pro" />
-		<br />
-		<label for="billing">Monthly / Yearly</label>
-		<input type="checkbox" name="billing" id="billing" />
+		{#each plans as plan (plan.id)}
+			<label>
+				<input
+					class={'radio-button'}
+					type={'radio'}
+					name={'plan'}
+					id={plan.id}
+					value={plan.id}
+					checked={$subscription.plan.id === plan.id}
+					on:change={() => subscription.changePlan(plan)}
+				/>
+				{plan.props.name} ({$subscription.billing === 'monthly'
+					? '$' + plan.props.monthlyPrice + '/mo'
+					: '$' + plan.props.yearlyPrice + '/yr'})
+			</label>
+		{/each}
+		<div class="billing-toggle">
+			<div
+				class={$subscription.billing === 'monthly' ? 'text-msf-marine-blue' : 'text-msf-cool-gray'}
+			>
+				{'Monthly'}
+			</div>
+			<label class={'relative inline-flex cursor-pointer items-center'}>
+				<input
+					class="peer sr-only"
+					type={'checkbox'}
+					name={'billing'}
+					id={'billing'}
+					checked={$subscription.billing === 'yearly'}
+					on:change={() =>
+						subscription.changeBilling($subscription.billing === 'monthly' ? 'yearly' : 'monthly')}
+				/>
+				<div
+					class={`peer h-6 w-11 rounded-full bg-msf-marine-blue after:absolute
+          after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 
+          after:bg-white after:transition-all after:content-[''] peer-checked:bg-msf-marine-blue peer-checked:after:translate-x-full 
+          peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 
+          `}
+				/>
+			</label>
+			<div
+				class={$subscription.billing === 'yearly' ? 'text-msf-marine-blue' : 'text-msf-cool-gray'}
+			>
+				{'Yearly'}
+			</div>
+		</div>
 	</form>
 </section>
+
+<style lang="postcss">
+	form {
+		@apply flex flex-col gap-5;
+	}
+
+	.billing-toggle {
+		@apply flex w-full items-center justify-center gap-5 rounded-md bg-msf-alabaster p-3;
+	}
+
+	.billing-toggle > div {
+		@apply font-msf-font-medium;
+	}
+
+	.radio-button {
+		@apply h-full w-full p-5;
+	}
+</style>
---
+++ src/lib/stores/subscription-store.ts
@@ -0,0 +1,112 @@
+import { browser } from '$app/environment';
+import { writable } from 'svelte/store';
+
+type AddOn = {
+	id: 'online_service' | 'larger_storage' | 'custom_profile';
+	props: {
+		name: string;
+		desc: string;
+		monthlyPrice: number;
+		yearlyPrice: number;
+	};
+};
+
+type Plan = {
+	id: 'arcade' | 'advanced' | 'pro';
+	props: {
+		name: string;
+		monthlyPrice: number;
+		yearlyPrice: number;
+	};
+};
+
+type Subscription = {
+	plan: Plan;
+	addons: AddOn[];
+	billing: 'monthly' | 'yearly';
+};
+
+const addOns: AddOn[] = [
+	{
+		id: 'online_service',
+		props: {
+			name: 'Online service',
+			desc: 'Access to multiplayer games',
+			monthlyPrice: 1,
+			yearlyPrice: 10
+		}
+	},
+	{
+		id: 'larger_storage',
+		props: {
+			name: 'Larger storage',
+			desc: 'Extra 1TB of cloud save',
+			monthlyPrice: 2,
+			yearlyPrice: 20
+		}
+	},
+	{
+		id: 'custom_profile',
+		props: {
+			name: 'Customizable profile',
+			desc: 'Custom theme on your profile',
+			monthlyPrice: 2,
+			yearlyPrice: 20
+		}
+	}
+];
+
+const plans: Plan[] = [
+	{ id: 'arcade', props: { name: 'Arcade', monthlyPrice: 9, yearlyPrice: 90 } },
+	{ id: 'advanced', props: { name: 'Advanced', monthlyPrice: 12, yearlyPrice: 120 } },
+	{ id: 'pro', props: { name: 'Pro', monthlyPrice: 15, yearlyPrice: 150 } }
+];
+
+const defaultValue: Subscription = {
+	plan: plans[0],
+	addons: [],
+	billing: 'monthly'
+};
+
+const createPlanStore = () => {
+	let initialPlan = defaultValue;
+
+	if (browser) {
+		const storedPlan = localStorage.getItem('subscription');
+		initialPlan = storedPlan ? JSON.parse(storedPlan) : defaultValue;
+	}
+
+	const { subscribe, update } = writable<Subscription>(initialPlan);
+
+	const changePlan = (plan: Plan) =>
+		update((value) => {
+			const newSubscription = { plan, addons: value.addons, billing: value.billing };
+			if (browser) localStorage.setItem('subscription', JSON.stringify(newSubscription));
+			return newSubscription;
+		});
+
+	const changeAddOns = (addons: AddOn[]) =>
+		update((value) => {
+			const newSubscription = { plan: value.plan, addons, billing: value.billing };
+			if (browser) localStorage.setItem('subscription', JSON.stringify(newSubscription));
+			return newSubscription;
+		});
+
+	const changeBilling = (billing: 'monthly' | 'yearly') =>
+		update((value) => {
+			const newSubscription = { plan: value.plan, addons: value.addons, billing };
+			if (browser) localStorage.setItem('subscription', JSON.stringify(newSubscription));
+			return newSubscription;
+		});
+
+	return {
+		subscribe,
+		changePlan,
+		changeAddOns,
+		changeBilling
+	};
+};
+
+const subscription = createPlanStore();
+
+export { addOns, plans, subscription };
