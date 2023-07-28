18808d0b23f99098cd5e9f52d391044dda3ca3d8
mdeme01 <se4n01@gmail.com>
2023-07-28 09:33:55 +0200

    feat: add assets and styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,1 @@
-<script lang="ts">
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Multi-step form</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'msf-marine-blue': 'hsl(213, 96%, 18%)',
+				'msf-purplish-blue': 'hsl(243, 100%, 62%)',
+				'msf-pastel-blue': 'hsl(228, 100%, 84%)',
+				'msf-light-blue': 'hsl(206, 94%, 87%)',
+				'msf-strawberry-red': 'hsl(354, 84%, 57%)',
+				'msf-cool-gray': 'hsl(231, 11%, 63%)',
+				'msf-light-gray': 'hsl(229, 24%, 87%)',
+				'msf-magnolia': 'hsl(217, 100%, 97%)',
+				'msf-alabaster': 'hsl(231, 100%, 99%)',
+				'msf-white': 'hsl(0, 0%, 100%)'
+			},
+			fontSize: {
+				'msf-font-size': '16px'
+			},
+			fontFamily: {
+				'msf-font-family': ['Ubuntu', 'sans-serif']
+			},
+			fontWeight: {
+				'msf-font-regular': '400',
+				'msf-font-medium': '500',
+				'msf-font-bold': '700'
+			}
+		}
 	},
 	plugins: []
 };
