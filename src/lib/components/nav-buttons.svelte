06f794e279c3464541ffcb75b2f343f4f5595b3c
mdeme01 <se4n01@gmail.com>
2023-07-28 14:12:30 +0200

    feat: nav buttons styling

--- src/lib/components/nav-buttons.svelte
+++ src/lib/components/nav-buttons.svelte
@@ -2,11 +2,39 @@
 	import { step } from '$lib/stores/step-store';
 </script>
 
-<nav>
-	{#if $step !== 5}
-		<button on:click={() => step.updateStep('dec')} hidden={$step === 1}>Go Back</button>
-		<button on:click={() => step.updateStep('inc')}>{$step === 4 ? 'Confirm' : 'Next step'}</button>
+<nav class={$step === 1 ? 'justify-end' : $step === 5 ? 'justify-center' : 'justify-between'}>
+	{#if $step === 5}
+		<button on:click={() => step.updateStep('res')} class={'btn-confirm'}>{'Reset'}</button>
 	{:else}
-		<button on:click={() => step.updateStep('res')}>Reset</button>
+		{#if $step > 1}
+			<button on:click={() => step.updateStep('dec')} class={'btn-back'}>{'Go Back'}</button>
+		{/if}
+		{#if $step === 4}
+			<button on:click={() => step.updateStep('inc')} class={'btn-confirm'}>{'Confirm'}</button>
+		{:else}
+			<button on:click={() => step.updateStep('inc')} class={'btn-next'}>{'Next Step'}</button>
+		{/if}
 	{/if}
 </nav>
+
+<style lang="postcss">
+	nav {
+		@apply absolute bottom-0 flex w-full items-center bg-msf-white p-5 sm:static;
+	}
+
+	button {
+		@apply cursor-pointer rounded-md pb-3 pl-5 pr-5 pt-3 font-msf-font-medium;
+	}
+
+	.btn-back {
+		@apply text-msf-cool-gray hover:text-msf-marine-blue;
+	}
+
+	.btn-next {
+		@apply bg-msf-marine-blue text-msf-white hover:bg-msf-marine-blue/80;
+	}
+
+	.btn-confirm {
+		@apply bg-msf-purplish-blue text-msf-white hover:bg-msf-purplish-blue/80;
+	}
+</style>
