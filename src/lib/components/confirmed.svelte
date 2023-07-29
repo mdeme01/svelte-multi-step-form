148b26b9978e17c831379338c50efda8b85b0c6b
mdeme01 <se4n01@gmail.com>
2023-07-29 23:39:46 +0200

    fix: layout styling

--- src/lib/components/confirmed.svelte
+++ src/lib/components/confirmed.svelte
@@ -2,16 +2,16 @@
 	import { step } from '$lib/stores/step-store';
 </script>
 
-<div class="form-step place-self-center">
-	<section class="form-content">
-		<div class="flex h-full flex-col items-center justify-center">
+<div class="form-step md:place-self-center">
+	<section class="form-content text-center">
+		<div class="mt-20 flex justify-center">
 			<img src="/images/icon-thank-you.svg" alt="checkmark" />
-			<h1 class="mt-5">Thank you!</h1>
-			<p class="text-center">
-				Thanks for confirming your subscription! We hope you have fun using our platform. If you
-				ever need support, please feel free to email us at support@loremgaming.com
-			</p>
 		</div>
+		<h1 class="mb-5 mt-5">Thank you!</h1>
+		<p class="mb-16 text-msf-cool-gray">
+			Thanks for confirming your subscription! We hope you have fun using our platform. If you ever
+			need support, please feel free to email us at support@loremgaming.com
+		</p>
 	</section>
 	<nav class="form-button-wrapper justify-center">
 		<button on:click={() => step.updateStep('res')} class={'btn-confirm'}>{'Reset'}</button>
