b6db7b0cad899b40090a5b7098cfccbf795ea9dd
mdeme01 <se4n01@gmail.com>
2023-07-30 09:27:55 +0200

    feat: invalid input styling

--- src/lib/components/personal-info.svelte
+++ src/lib/components/personal-info.svelte
@@ -1,46 +1,65 @@
 <script lang="ts">
 	import { personalInfo } from '$lib/stores/personal-info-store';
 	import { step } from '$lib/stores/step-store';
+
+	let nameError = false;
+	let emailError = false;
+	let phoneError = false;
 </script>
 
-<form on:submit={() => step.updateStep('inc')} class="form-step">
+<form on:submit|preventDefault={() => step.updateStep('inc')} class="form-step">
 	<section class="form-content">
 		<h1>Personal info</h1>
 		<p class="mb-8 text-msf-cool-gray">Please provide your name, email address and phone number.</p>
 		<label>
-			<p>Name</p>
+			<div class="flex items-center justify-between">
+				<p>Name</p>
+				<p hidden={!nameError} class="font-bold text-msf-strawberry-red">This field is required</p>
+			</div>
 			<input
 				required
 				type="text"
-				name="name"
 				id="name"
 				placeholder="e.g. Stephen King"
 				bind:value={$personalInfo.name}
-				class="mb-5 w-full"
+				on:invalid={() => (nameError = true)}
+				class={nameError
+					? 'text-input border-msf-strawberry-red focus:border-msf-strawberry-red'
+					: 'text-input border-msf-cool-gray/60 focus:border-msf-purplish-blue'}
 			/>
 		</label>
 		<label class="mb-5">
-			<p>Email</p>
+			<div class="flex items-center justify-between">
+				<p>Email</p>
+				<p hidden={!emailError} class="font-bold text-msf-strawberry-red">This field is required</p>
+			</div>
 			<input
 				required
 				type="email"
-				name="email"
 				id="email"
 				placeholder="e.g. stephenking@lorem.com"
 				bind:value={$personalInfo.email}
-				class="mb-5 w-full"
+				on:invalid={() => (emailError = true)}
+				class={emailError
+					? 'text-input border-msf-strawberry-red focus:border-msf-strawberry-red'
+					: 'text-input border-msf-cool-gray/60 focus:border-msf-purplish-blue'}
 			/>
 		</label>
 		<label>
-			<p>Phone Number</p>
+			<div class="flex items-center justify-between">
+				<p>Phone Number</p>
+				<p hidden={!phoneError} class="font-bold text-msf-strawberry-red">This field is required</p>
+			</div>
 			<input
 				required
 				type="tel"
-				name="phone"
 				id="phone"
 				placeholder="e.g +1 234 567 890"
 				bind:value={$personalInfo.phone}
-				class="mb-5 w-full"
+				on:invalid={() => (phoneError = true)}
+				class={phoneError
+					? 'text-input border-msf-strawberry-red focus:border-msf-strawberry-red'
+					: 'text-input border-msf-cool-gray/60 focus:border-msf-purplish-blue'}
 			/>
 		</label>
 	</section>
